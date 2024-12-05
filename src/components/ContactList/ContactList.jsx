d889cb5612e030e945cf5e0d2829fd91d0c2f1cd
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2024-12-05 15:52:56 +0200

    Updated ContactList with selectFilteredContacts from createSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,19 @@
 import { useSelector } from 'react-redux';
 import s from './ContactList.module.css';
 import Contact from '../Contact/Contact';
-import { selectContacts } from '../../redux/contactsSlice';
-import { selectFilter } from '../../redux/filtersSlice';
+// import { selectContacts } from '../../redux/contactsSlice';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 
 
 const ContactList = () => {
    
-    const contacts = useSelector(selectContacts);
-    const filter = useSelector(selectFilter);
-    const filteredData = contacts.filter(item=> item.name.toLowerCase().includes(filter.toLowerCase()))
+    // const contacts = useSelector(selectContacts);
+    const filter = useSelector(selectFilteredContacts);
+    // const filteredData = contacts.filter(item=> item.name.toLowerCase().includes(filter.toLowerCase()))
     return (
         <div className={s.container}>
             <ul className={s.wrapper}>
-                {filteredData.map((contact) => (
+                {filter.map((contact) => (
                     <li key={contact.id} className={s.item}>
                         <Contact contact={contact} id={contact.id} />
                     </li>
