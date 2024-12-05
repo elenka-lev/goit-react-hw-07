f25439e6ad40c4fcf26c66763b80e8fa90723e4d
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2024-12-05 15:43:03 +0200

    Added createSelector

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,12 @@
-import { createSlice, isAnyOf } from "@reduxjs/toolkit"
+import { createSelector, createSlice, isAnyOf } from "@reduxjs/toolkit"
 import { deleteContact, fetchContacts, addContact } from "./contactsOps";
+import { selectFilter } from "./filtersSlice";
 
 
 const initialState = {
     items: [],
-    isLoading: false,
-    isError: false,
+    loading: false,
+    error: false,
 }
 
 const slice = createSlice({
@@ -17,7 +18,7 @@ const slice = createSlice({
                 state.items = action.payload;
             })
             .addCase(fetchContacts.pending, (state) => {
-                state.isLoading = true;
+                state.loading = true;
             })
             .addCase(deleteContact.fulfilled, (state, action) => {
                 state.items = state.items.filter(item => item.id !== action.payload.id)
@@ -26,22 +27,34 @@ const slice = createSlice({
                 state.items.push(action.payload)
             })
             .addMatcher(isAnyOf(addContact.pending, fetchContacts.pending, deleteContact.pending), state => {
-                state.isError = false;
-                state.isLoading = true;
+                state.error = false;
+                state.loading = true;
             })
             .addMatcher(isAnyOf(addContact.rejected, fetchContacts.rejected, deleteContact.rejected), state => {
-                state.isLoading = false;
-                state.isError = true;
+                state.loading = false;
+                state.error = true;
             })
             .addMatcher(isAnyOf(addContact.fulfilled, fetchContacts.fulfilled, deleteContact.fulfilled), state => {
-                state.isLoading = false;
-                state.isError = false;
+                state.loading = false;
+                state.error = false;
             });
     }
 })
 
+
+
 export const selectContacts = state => state.contacts.items;
-export const selectIsError = state => state.contacts.isError;
-export const selectIsLoading = state => state.contacts.isLoading;
+export const selectIsError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.loading;
+
+//мемоізація селекторів
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 
 export const contactReducer = slice.reducer; //іде у стор
